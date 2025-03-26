7b37923e97b56c1635afc99ce21ca5175bd77157
Thanajor <thanajor@outlook.com>
2025-03-26 11:00:55 -0500

    feat: add initial setup and plugin manager

---
+++ lua/options.lua
@@ -0,0 +1,13 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.signcolumn = 'yes'
+
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+-- Decrease mapped sequence wait time
+vim.opt.timeoutlen = 300
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
