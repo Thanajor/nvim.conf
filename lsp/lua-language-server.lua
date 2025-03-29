6e9da70c57ff5f7bb985aee03261747df5da588b
Thanajor <thanajor@outlook.com>
2025-03-28 23:55:31 -0500

    feat: add some lsp config

---
+++ lsp/lua-language-server.lua
@@ -0,0 +1,12 @@
+return {
+	filetypes = {"lua"},
+	cmd = {"lua-language-server"},
+	root_markers = {".luarc.json"},
+	settings = {
+		Lua = {
+			telemetry = {
+				enable = false
+			}
+		}
+	}
+}
---
+++ lsp/tinymist.lua
@@ -0,0 +1,8 @@
+return {
+	filetypes = {'typst'},
+	cmd = {'tinymist'},
+	settings = {
+		exportPdf = 'onDocumentHasTitle',
+		-- formatterMode = "typstyle",
+	}
+}
