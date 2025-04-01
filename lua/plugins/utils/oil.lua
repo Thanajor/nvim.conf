de6bb778560eaec32271166807617a5e78e4c8d4
Thanajor <thanajor@outlook.com>
2025-03-31 22:16:55 -0500

    chore: remove dependecie mini icon

--- lua/plugins/utils/oil.lua
+++ lua/plugins/utils/oil.lua
@@ -30,9 +30,7 @@ keymaps = {
     ["g\\"] = { "actions.toggle_trash", mode = "n" },
   },
   },
-  -- Optional dependencies
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
 
