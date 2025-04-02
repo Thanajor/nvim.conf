feb536de43553880e26057d3ebe7e51174258b63
Thanajor <thanajor@outlook.com>
2025-04-01 21:58:10 -0500

    chore(ui): add visual marks and icon config

---
+++ lua/plugins/ui/nvim-web-devicons.lua
@@ -0,0 +1,13 @@
+return {
+	"nvim-tree/nvim-web-devicons",
+	opts = {
+		override = {
+			typ = {
+				icon = "",
+				color = "#00afc6",
+				cterm_color = "65",
+				name = "Typst"
+			}
+		};
+	}
+}
