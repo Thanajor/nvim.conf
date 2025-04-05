31edd3e4ab0f031d0e00d4d73a3e5c8a23ff5382
Thanajor <thanajor@outlook.com>
2025-04-04 22:01:09 -0500

    fix(python): fix configuration for lsp basepyright

--- lsp/basedpyright.lua
+++ lsp/basedpyright.lua
@@ -1,6 +1,16 @@
 return {
- filetypes = { "python" },
- cmd = { "basedpyright", "--stdio" },
- root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", "pyrightconfig.json" },
- -- settings = {}
+	filetypes = { "python" },
+	cmd = { "basedpyright-langserver", "--stdio" },
+	root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", "pyrightconfig.json" },
+	settings = {
+		typeCheckingMode = "recommended",
+		basedpyright = {
+			analysis = {
+				diagnosticMode = "openFilesOnly",
+				inlayHints = {
+					callArgumentNames = true
+				}
+			}
+		}
+	}
 }
