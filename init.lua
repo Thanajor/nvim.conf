972954ee19dc8eca317d5c664f9644de8a2ceed5
Thanajor <thanajor@outlook.com>
2025-04-14 22:05:11 -0500

    chore(lsp): add loop to automatically enable LSP server

--- init.lua
+++ init.lua
@@ -5,3 +5,11 @@ require("config.keymaps")
 require("config.autocmds")
 require("config.options")
 require("config.clipboard")
+
+local lsp_path = vim.fn.stdpath("config") .. "/lsp"
+for _, file in ipairs(vim.fn.readdir(lsp_path)) do
+	local server_name = file:match("^(.*)%.lua$")
+	if server_name then
+		vim.lsp.enable(server_name)
+	end
+end
