f0304b3df17d18a27bb5b7d57f937736c733850c
Thanajor <thanajor@outlook.com>
2025-05-06 21:02:12 -0500

    chore(ui): changes somes colors

--- lua/plugins/ui/tokyonight.lua
+++ lua/plugins/ui/tokyonight.lua
@@ -1,11 +1,28 @@
 return {
-	'folke/tokyonight.nvim',
-	priority = 1000, -- Make sure to load this before all the other start plugins.
-	opts = {
-		style = "storm",
-		-- transparent = true,
-
-	init = function()
-		vim.cmd [[colorscheme tokyonight]]
-	end,
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {
+        style = "storm",
+        styles = {
+            sidebars = "transparent",
+            floats = "transparent"
+        },
+        -- transparent = true,
+        on_highlights = function(hl, c)
+            hl.IncSearch = {
+                bg = c.blue1,
+                fg = c.bg
+            }
+            hl.FlashLabel = {
+                bold = true,
+                bg = c.teal,
+                fg = c.bg
+            }
+            hl.CursorLineNr = { bold = true, fg = c.blue1 }
+        end,
+        cache = true,
+    },
+    init = function()
+        vim.cmd [[colorscheme tokyonight]]
+    end
 }
