301583cdfb2f528ff5d9487bd3cb4df80d6a9a53
Thanajor <thanajor@outlook.com>
2025-06-30 16:11:26 -0500

    feat(lsp): add keymaps to select nodes with treesitter

--- lua/plugins/lsp/treesitter.lua
+++ lua/plugins/lsp/treesitter.lua
@@ -5,6 +5,14 @@ return {
 	main = "nvim-treesitter.configs",
 	opts = {
 		ensure_installed = { "markdown", "markdown_inline", "rust", "python", "typst", "nix" },
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "gnn",
+				node_incremental = "grn",
+				scope_incremental = "grc",
+				node_decremental = "grm",
+			},
 		},
 	},
 }
