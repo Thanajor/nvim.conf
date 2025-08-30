b2639459e05f65d696e182c65997e8f6f82fc6a8
Thanajor <thanajor@outlook.com>
2025-08-29 21:50:09 -0500

    feat(lsp): resolve tinymist rootPath in typst-preview

--- lua/plugins/typst-preview.lua
+++ lua/plugins/typst-preview.lua
@@ -1,5 +1,21 @@
+local function tinymist_root()
+	local clients = vim.lsp.get_clients({ bufnr = 0 })
+
+	---@type vim.lsp.Client|nil
+	local tinymist = vim.iter(clients)
+		:find(function(client)
+			return client.name == "tinymist"
+		end)
+
+	return tinymist
+		and tinymist.config
+		and tinymist.config.settings
+		and tinymist.config.settings.rootPath
+end
+
 return {
 	'chomosuke/typst-preview.nvim',
+	event = "LspAttach",
 	ft = 'typst',
 	version = '1.*',
 	opts = {
@@ -9,5 +25,15 @@ return {
 			['tinymist'] = 'tinymist',
 			['websocat'] = 'websocat',
 		},
+		get_root = function(path_of_main_file)
+			local root = tinymist_root()
+			if root then
+				return root
+			else
+				local path = vim.fn.fnamemodify(path_of_main_file, ':p:h')
+                vim.notify("Could not get Tinymist rootPath; defaulting to" .. path, vim.log.levels.WARN)
+				return path
+			end
+		end
 	}, -- lazy.nvim will implicitly calls `setup {}`
 }
