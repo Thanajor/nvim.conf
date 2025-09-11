3d74f22cfd16af46ffd4604c3601cdb0ee761888
Thanajor <thanajor@outlook.com>
2025-09-11 07:55:42 -0500

    feat(dap): initial Rust DAP config

--- lua/plugins/lsp/dap.lua
+++ lua/plugins/lsp/dap.lua
@@ -1,4 +1,41 @@
 return {
-	{ "mfussenegger/nvim-dap" },
-	{ "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } }
+	"mfussenegger/nvim-dap",
+	dependencies = { "rcarriga/nvim-dap-ui", "nvim-neotest/nvim-nio" },
+
+	config = function()
+		local dap, dapui = require("dap"), require("dapui")
+
+		dap.adapters.lldb = {
+			type = 'executable',
+			command = 'lldb-dap',
+			name = 'lldb'
+		}
+
+		dap.configurations.rust = {
+			{
+				name = 'Launch',
+				type = 'lldb',
+				request = 'launch',
+				program = function()
+					return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+				end,
+				cwd = '${workspaceFolder}',
+				stopOnEntry = false,
+				args = {},
+			}
+		}
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+	end,
 }
